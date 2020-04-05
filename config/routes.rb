ce9c16cf07ee5502927d6717b703bd0b8a63ea5a
ryota.tomiyama <lit.karu.noir@gmail.com>
2020-04-05 15:48:59 +0000

    resouces :messages の中身

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # CRUD
+  get 'messages/:id', to: 'messages#show'
+  post 'messages', to: 'messages#create'
+  put 'messages/:id', to: 'messages#update'
+  delete 'messages/:id', to: 'messages#destroy'
+
+  # index: showの補助ページ
+  get 'messages', to: 'messages#index'
+
+  # new: 新規作成用のフォームページ
+  get 'messages/new', to: ''
+
+  # edit: 更新用のフォームページ
+  get 'messages/:id/edit', to: 'messages#edit'
 end
