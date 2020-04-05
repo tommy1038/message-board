d301424a55eef743b00d1ff58ab32f3e7b90ded8
ryota.tomiyama <lit.karu.noir@gmail.com>
2020-04-05 15:53:01 +0000

    apply resources :messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-  # CRUD
-  get 'messages/:id', to: 'messages#show'
-  post 'messages', to: 'messages#create'
-  put 'messages/:id', to: 'messages#update'
-  delete 'messages/:id', to: 'messages#destroy'
+  root to: 'messages#index'
 
-  # index: showの補助ページ
-  get 'messages', to: 'messages#index'
-
-  # new: 新規作成用のフォームページ
-  get 'messages/new', to: ''
-
-  # edit: 更新用のフォームページ
-  get 'messages/:id/edit', to: 'messages#edit'
+  resources :messages
 end
